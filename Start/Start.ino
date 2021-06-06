f19bfa04a5465a3db167b40c340ff19f21c5819b
Mickey <mickey_fraser@outlook.com>
2021-06-05 20:28:11 -0500

    Minutes Seconds & 3 minute switch

--- Start/Start.ino
+++ Start/Start.ino
@@ -1,15 +1,15 @@
 
 
-
-
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
-#include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
   
 int ThreeOrFiveMinuteSwitch = 2;
 const int GoButton = 1;
 int HornButton = 2;
 bool running = false;
+bool goButtonPressed = false;
+bool isFiveMinute;
+
+int brightness = 1;
 
 Adafruit_7segment display = Adafruit_7segment();
 
@@ -26,28 +26,44 @@ void setup() {
   pinMode(HornButton, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  if(digitalRead(ThreeOrFiveMinuteSwitch) == HIGH) {
+  isFiveMinute = digitalRead(ThreeOrFiveMinuteSwitch);
+  if(isFiveMinute) {
     millisToZero = 330 * 1000 ; // five and a half minutes
   } else {
     millisToZero = 210 * 1000; // three and a half minutes
-  }
-  
+  }    
+
   display.begin(0x70);
+  display.setBrightness(brightness);
 }
 
 void loop() {
-  if(digitalRead(GoButton) == LOW) {
-    toggleState();
-  }
+  readInput();
 
   if(running) {
     updateTime();
   }
-
+  writeTime();
 
   delay(100);
-  Serial.println(millisToZero);
+}
 
+void readInput(){
+  checkGoButton();
+  if(!running){
+    checkTimeMode();
+  }
+}
+
+void checkGoButton(){
+  if(digitalRead(GoButton) == LOW){
+    if( !goButtonPressed ) {
+      goButtonPressed = true;
+      toggleState();
+    }
+  } else {
+    goButtonPressed = false;
+  }
 }
 
 void toggleState(){
@@ -59,20 +75,37 @@ void toggleState(){
       millisLastChecked = millis();
   }
 }
+
+void checkTimeMode(){
+  
+  if(digitalRead(ThreeOrFiveMinuteSwitch) != isFiveMinute){
+    isFiveMinute = digitalRead(ThreeOrFiveMinuteSwitch);
+    if(isFiveMinute) {
+      millisToZero = 330 * 1000 ; // five and a half minutes
+    } else {
+      millisToZero = 210 * 1000; // three and a half minutes
+    }    
+  }
+}
 void updateTime(){
     millisCurrent = millis();
     if(millisCurrent > millisLastChecked) {
       millisToZero = millisToZero - (millisCurrent - millisLastChecked);
       millisLastChecked = millisCurrent;
     }
-    writeTime();
 }
 void writeTime(){
-    display.writeDigitNum(0, (millisToZero / 1000000), false);
-    display.writeDigitNum(1, (millisToZero / 100000) % 10, false);
+    display.writeDigitNum(1, getMinutes(), false);
     display.drawColon(true);
-    display.writeDigitNum(3, (millisToZero / 10000) % 10, false);
-    display.writeDigitNum(4, (millisToZero / 1000) % 10, false);
+    display.writeDigitNum(3, (getSeconds() / 10) % 10, false);
+    display.writeDigitNum(4, getSeconds() % 10, false);
     display.writeDisplay();
 }
 
+int getMinutes(){
+  return (millisToZero / 1000 / 60);
+}
+
+int getSeconds(){
+  return (millisToZero / 1000 ) % 60;
+}
